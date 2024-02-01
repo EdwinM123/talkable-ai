1d4d9c1de32654e4a5459a265a637065788f5f46
EdwinM123 <152922530+EdwinM123@users.noreply.github.com>
2024-02-01 17:35:38 +0000

    im cooked

--- code/main.py
+++ code/main.py
@@ -1,4 +1,11 @@
-include pyttsx3
-include SpeechRecognition 
-include os 
+from datetime import datetime
+import pyttsx3
+import speech_recognition as sr 
+import webbrowser
+import wikipedia 
+import wolframalpha
 
+#speech recognition 
+software = pyttsx3.init();
+voices = software.getProperty("voices");
+software.setProperty('voices', voices[0]);
