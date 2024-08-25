0d814c8daa11cac2c8ccd3fc0dcd52a4b7c621d3
EdwinM123 <edwinamunoz357@gmail.com>
2024-08-25 07:32:05 -0500

    I'm restarting this!!

--- code/main.py
+++ code/main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import pyttsx3
 import speech_recognition as sr 
 import webbrowser
-import wikipedia 
+import wikipedia  
  
 #browser stuff
 chrome_path = r"C:\Program Files\Google\Chrome\Application\Chrome.exe" 
@@ -11,38 +11,9 @@ webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
 #speech recognition 
 engine = pyttsx3.init();
 voices = engine.getProperty("voices");
-engine.setProperty('voices', voices[0].id); #0=guy 1=girl
+engine.setProperty('voices', voices[1].id); #0=guy 1=girl
 #word to activate 
-word = 'karen'
-
-#wolfram alpha 
-wolframId = '4PQE2E-Y62UYAV8LP'
-wolframClient = wolframalpha.Client(wolframId);
-
-def check_result_wolfram(var):
-    ###List
-    if isinstance(var, list):
-        return var[0]['plaintext'];
-    ###Dictionary
-    else:
-        return var['plaintext']
-
-def search_wolframalpha(query=''):
-    response = wolframClient.query(query)
-
-    if response['@success'] == 'false':
-        return 'could not compute';
-    else: 
-        pod0 = response['pod'][0]
-        pod1=response['pop'][1]
-        if(('result') in pod1['@title'].lower()) or (pod1.get('@primary', 'false') == 'true') or ('definition' in pod1['@title'].lower()):
-            result = check_result_wolfram(pod1['subpod'])
-            return result.split('(')[0];
-        else:
-            question = check_result_wolfram(pod0['subpod'])
-            return question.split('(')[0]
-            speak('could not find question, checking wikipedia')
-            return search_wikipedia(question);
+word = 'karen' 
 
 def speak(text, rate =120):
     engine.setProperty('rate', rate);
@@ -111,11 +82,13 @@ if __name__ == '__main__':
                 speak("searching...")
                 speak(search_wikipedia(query));
             #wolfram questions
-            if query[0] == 'calculate' or query[0] == 'compute' or query[0] == 'computer':
+            if query[0] == 'calculate' or query[0] == 'compute' or query[0] == 'computer' or query[0] == 'check':
                 query = ' '.join(query[1:]);
                 speak("calculating...")
                 try: 
-                    result = search_wolframalpha(query)
+                    result = search_wikipedia(query)
+                    speak(result) 
+                    result = webbrowser.get('chrome').open_new(query)
                     speak(result)
                 except:
                     speak('Unable to compute.')
