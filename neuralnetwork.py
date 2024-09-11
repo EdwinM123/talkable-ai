8cf5fae051c61aacd37c8374687ab7c38b1dc224
EdwinM123 <152922530+EdwinM123@users.noreply.github.com>
2024-09-11 14:30:04 +0000

    Uh Oh

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -2,7 +2,7 @@ import os
 import torch 
 from torch import nn 
 from torch.utils.data import DataLoader 
-from torchvision import datasets, transforms
+#from torchvision import datasets, transforms
 
 device=(
     "cuda" 
